89aed849ccf7d1095becdc6bf53ec7a68f8248f8
devMunyi <samunyi90@gmail.com>
2023-01-27 09:21:43 +0300

    Add a message to show when no search match found

--- extension.js
+++ extension.js
@@ -1,39 +1,39 @@
 const vscode = require('vscode');
 const axios = require('axios');
-const open = require('open')
+const open = require('open');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 
 var __awaiter =
-(this && this.__awaiter) ||
-function (thisArg, _arguments, P, generator) {
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
       }
-    }
-    function rejected(value) {
-      try {
-        step(generator['throw'](value));
-      } catch (e) {
-        reject(e);
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
       }
-    }
-    function step(result) {
-      result.done
-        ? resolve(result.value)
-        : new P(function (resolve) {
-            resolve(result.value);
-          }).then(fulfilled, rejected);
-    }
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : new P(function (resolve) {
+              resolve(result.value);
+            }).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
 
 //function that activates the extension
 function activate(context) {
@@ -71,43 +71,61 @@ function deactivate() {}
 // function to execute a search based on search term
 function executeSearch(searchTerm) {
   return __awaiter(this, void 0, void 0, function* () {
-      if (!searchTerm || searchTerm.trim() === '') {
-          return;
-      }
-      searchTerm = searchTerm.trim();
-      console.log(`User initiated a zidi search with [${searchTerm}] search term`);
+    if (!searchTerm || searchTerm.trim() === '') {
+      return;
+    }
+    searchTerm = searchTerm.trim();
+    console.log(
+      `User initiated a zidi search with [${searchTerm}] search term`
+    );
 
-      const rpp = 250;
-      const apiSearchUrl = `https://backgen.net/back/search-codesnippet?search_=${searchTerm}&rpp=${rpp}`
-      const zidiSiteSearchUrl = `https://zidiapp.com?search_=${searchTerm}`;
-      const googleSearchUrl = `https://www.google.com/search?q=${searchTerm}`;
+    const rpp = 250;
+    const apiSearchUrl = `https://backgen.net/back/search-codesnippet?search_=${searchTerm}&rpp=${rpp}`;
+    const zidiSiteSearchUrl = `https://zidiapp.com?search_=${searchTerm}`;
+    const googleSearchUrl = `https://www.google.com/search?q=${searchTerm}`;
 
-      const questionsMeta = [
-          { title: `🌐 🔎 Search On ZIDI Site: ${searchTerm}`, url:  zidiSiteSearchUrl},
-          { title: `🕸️ 🔎 Search On Google: ${searchTerm}`, url: googleSearchUrl },
-      ];
-      try {
-          const searchResponse = (yield axios.get(apiSearchUrl)).data;
-          if (searchResponse.data && searchResponse.data.length > 0) {
-              searchResponse.data.forEach(({ uid, title, language_name, user_implementation_type }, i) => {
-                  const hyphenatedTitle = hyphenateTitle(title)
-                  questionsMeta.push({
-                      title: `${i + 1} ➡️ ${title} : ✅${user_implementation_type}`,
-                      url: `https://zidiapp.com/solutions/${uid}/${hyphenatedTitle}`
-                  });
-              });
+    let questionsMeta = [];
+    try {
+      const searchResponse = (yield axios.get(apiSearchUrl)).data;
+      if (searchResponse.data && searchResponse.data.length > 0) {
+        searchResponse.data.forEach(
+          ({ uid, title, user_implementation_type }, i) => {
+            const hyphenatedTitle = hyphenateTitle(title);
+            questionsMeta.push({
+              title: `${i + 1} ➡️ ${title} : ✅${user_implementation_type}`,
+              url: `https://zidiapp.com/solutions/${uid}/${hyphenatedTitle}`,
+            });
           }
+        );
+      } else {
+        questionsMeta = [
+          { title: 'No Matches Found. Click esc key to dismiss and retry or click one of the following:' },
+          {
+            title: `🌐 🔎 Search On ZIDI Site: ${searchTerm}`,
+            url: zidiSiteSearchUrl,
+          },
+          {
+            title: `🕸️ 🔎 Search On Google: ${searchTerm}`,
+            url: googleSearchUrl,
+          },
+        ];
       }
-      catch (error) {
-          console.error(error);
-      }
-      const questions = questionsMeta.map(q => q.title);
-      const selectedTitle = yield vscode.window.showQuickPick(questions, { canPickMany: false });
-      const selectedQuestionMeta = questionsMeta.find(q => q.title === selectedTitle);
-      const selectedQuestionUrl = selectedQuestionMeta ? selectedQuestionMeta.url : null;
-      if (selectedQuestionUrl) {
-          open(selectedQuestionUrl);
-      }
+    } catch (error) {
+      console.error(error);
+    }
+    const questions = questionsMeta.map((q) => q.title);
+    const selectedTitle = yield vscode.window.showQuickPick(questions, {
+      canPickMany: false,
+    });
+    const selectedQuestionMeta = questionsMeta.find(
+      (q) => q.title === selectedTitle
+    );
+    const selectedQuestionUrl = selectedQuestionMeta
+      ? selectedQuestionMeta.url
+      : null;
+    if (selectedQuestionUrl) {
+      open(selectedQuestionUrl);
+    }
   });
 }
 
